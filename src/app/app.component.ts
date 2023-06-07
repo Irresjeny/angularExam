4e455e7308a39da1199372788659e8822954c5fc
gench <irresjeny@gmail.com>
2023-06-07 22:38:58 +0300

    1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,107 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+interface Image {
+  id: number;
+  webformatURL: string;
+}
+
+interface APODImage{
+  url:string;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Pixabay Gallery</h1>
+    <button mat-raised-button color="primary" (click)="loadImages('motorcycles')">Motorcycles</button>
+    <button mat-raised-button color="primary" (click)="loadImages('nature')">Nature</button>
+    <button mat-raised-button color="primary" (click)="loadImages('space')">Space</button>
+    <button mat-raised-button color="primary" (click)="loadImages('architecture')">Architecture</button>
+    <button mat-raised-button color="warn" (click)="openSettings()">Login</button>
+
+    <div *ngIf="currentTab === 'settings'" class="setting">
+      <h2>LOGIN</h2>
+      <mat-form-field class="example-full-width">
+        <mat-label>API Key Settings</mat-label>
+        <input [(ngModel)]="apiKey" matInput placeholder="Enter your API Key">
+      </mat-form-field>
+      <button mat-raised-button style="max-width: 500px" (click)="saveApiKey()">Save</button>
+      <h2>If you don't have api key follow the <a href="https://pixabay.com/ru/service/about/api/">link</a></h2>
+    </div>
+
+    <div *ngIf="currentTab !== 'settings'" >
+      <h2>{{ currentTab | uppercase }}</h2>
+      <div class="img-container">
+        <div *ngFor="let image of images" >
+          <img [src]="image.webformatURL" alt="Image"/>
+        </div>
+      </div>
+
+    </div>
+  `,
+  styles: [`
+    button {
+      margin-right: 10px;
+    }
+
+    img {
+      width: 200px;
+      height: 200px;
+      object-fit: cover;
+      margin: 5px;
+    }
+
+    .example-full-width {
+      min-width: 150px;
+      max-width: 500px;
+      width: 100%;
+    }
+
+    .setting{
+      display: flex;
+      flex-direction: column;
+    }
+
+    .img-container{
+      display: flex;
+      flex-wrap: wrap;
+        hight:'100px';
+    }
+  `]
 })
 export class AppComponent {
-  title = 'angularExam';
+  currentTab: string = '';
+  images: Image[] = [];
+  APODImages: APODImage[] = [];
+  apiKey: string = '';
+
+  constructor(private http: HttpClient) {
+    const savedApiKey = localStorage.getItem('apiKey');
+    if (savedApiKey) {
+      this.apiKey = savedApiKey;
+    }
+  }
+
+  openSettings() {
+    this.currentTab = 'settings';
+  }
+
+  loadImages(category: string) {
+    this.currentTab = category;
+    console.log("Category: " + category);
+    console.log("Current Tab: " + this.currentTab);
+    const timestamp = new Date().getTime();
+    const url = `https://pixabay.com/api/?key=${this.apiKey}&q=${category}&per_page=100`;
+    console.log("UR: " + url);
+    this.http.get<any>(url).subscribe(response => {
+      this.images = response.hits;
+      console.log("Response: " + response.hits);
+    });
+  }
+
+  saveApiKey() {
+    localStorage.setItem('apiKey', this.apiKey);
+    console.log("API Key: " + this.apiKey);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule, MatButtonModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
